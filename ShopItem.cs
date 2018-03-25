8ebf2123ccbb8fdd97829fbd03fe202bdcea3684
panzhenrong <pzr8023@outlook.com>
2018-03-25 19:17:01 +0800

    01

---
+++ ShopItem.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 商城歌曲物品实体类.
+/// </summary>
+public class ShopItem {
+
+    private string name;
+    public string Name
+    {
+        get { return name; }
+        set { value = name; }
+    }
+
+    private string lyric;
+    public string Lyric
+    {
+        get { return lyric; }
+        set { lyric = value; }
+    }
+
+    private float time;
+    public float Time 
+    {
+        get { return time; }
+        set { time = value; }
+    }
+
+    public ShopItem() { }
+    public ShopItem(string name, string lyric, float time)
+    {
+        this.name = name;
+        this.lyric = lyric;
+        this.time = time;
+    }
+
+    public override string ToString()
+    {
+        return string.Format("{0}:{1}:{2}", name, lyric, time);
+    }
+}
